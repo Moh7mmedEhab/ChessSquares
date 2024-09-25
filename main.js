b6513a6561a05b4ee06317c5b9b3f0e4ce0b06b8
Moh7mmedEhab <mohammed.ehab.23062@gmail.com>
2024-09-25 17:18:10 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+let input = document.querySelector("#square");
+
+let button = document.querySelector("#check");
+
+let result = document.querySelector("#result");
+
+let color = document.querySelector("#color");
+
+button.onclick = () => {
+  
+  let square = input.value;
+  
+  let column = square.toLowerCase().charCodeAt(0) - "a".charCodeAt(0);
+  
+  let row = parseInt(square[1])
+  
+  if (column > 7 || column < 0 || row > 8 || row < 1) {
+    
+    result.innerText = `invalid square`;
+    
+    color.style.backgroundColor = "transparent";
+    
+  } else {
+    
+    if ((row + column) % 2 == 1) {
+      
+      result.innerText = `${input.value} : Dark Square`;
+      
+      color.style.backgroundColor = "black";
+      
+    } else if ((row + column) % 2 == 0) {
+      
+      result.innerText = `${input.value} : light square`;
+      
+      color.style.backgroundColor = "#CCCC";
+      
+    } else {
+      
+      result.innerText = `invalid square`;
+    
+      color.style.backgroundColor = "transparent";
+      
+    }
+    
+  }
+  
+}
